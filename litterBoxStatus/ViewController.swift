fd4f3ed9220481fcc4aa4f503e18bcee8330cb75
salmeleh <salmeleh@gmail.com>
2016-12-07 11:06:33 -0600

    starting with VC

--- litterBoxStatus/ViewController.swift
+++ litterBoxStatus/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func resetButtonPressed(_ sender: Any) {
+        
+        
+        
     }
 
-
+    
+    
+    
 }
 
